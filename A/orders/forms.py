949aa1652ac892c90c86351a73315e7197f7c5dd
ali ramezani <66328048+neveralone0@users.noreply.github.com>
2022-09-18 18:38:16 +0430

    Delete A directory

--- A/orders/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-
-class CartAddForm(forms.Form):
-    quantity = forms.IntegerField(min_value=1, max_value=9)
-
-
-class CouponApplyForm(forms.Form):
-    code = forms.CharField()
