861eb4071bae4bc360dd54890d8924d1f35268c0
ali ramezani <66328048+neveralone0@users.noreply.github.com>
2023-02-01 21:29:56 +0330

    Delete home directory

--- home/context_processor.py
+++
@@ -1,7 +0,0 @@
-from .filters import ProductFilter
-from .models import Product
-
-
-def filter(request):
-    products = Product.objects.filter(available=True)
-    return {'filter': ProductFilter(request.GET, queryset=products)}
